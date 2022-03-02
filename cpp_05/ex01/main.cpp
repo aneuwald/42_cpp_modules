e69440545d7bbbf56354efddf0367b518d307bb4
aneuwald <aneuwald@gmail.com>
2022-03-02 16:03:58 +0000

    cpp_05 ex01

--- cpp_05/ex01/main.cpp
+++ cpp_05/ex01/main.cpp
@@ -6,55 +6,52 @@
 /*   By: acanterg <acanterg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 10:37:56 by acanterg          #+#    #+#             */
-/*   Updated: 2022/03/02 16:01:17 by acanterg         ###   ########.fr       */
+/*   Updated: 2022/03/02 16:02:47 by acanterg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main () {
 
 	std::cout << "===== DECLARATIONS =====" << std::endl;
-	Bureaucrat bure1;
-	Bureaucrat bure2("Theo");
-	Bureaucrat bure3(49);
-	Bureaucrat bure4("Philip", 150);
+	Bureaucrat bureaucrat("Philip", 120);
+	Form form("G12", 150, 150);
+	Form form2("F28", 64, 150);
 
 	std::cout << "\n===== PRINTING =====" << std::endl;
-	std::cout << bure1 << std::endl;
-	std::cout << bure2 << std::endl;
-	std::cout << bure3 << std::endl;
-	std::cout << bure4 << std::endl;
+	std::cout << bureaucrat << std::endl;
+	std::cout << form << std::endl;
+	std::cout << form2 << std::endl;
 
-	std::cout << "\n===== ERROR's =====" << std::endl;
+	std::cout << "\n===== SIGNING =====" << std::endl;
 	try {
-		bure1.decrementGrade(100);
-		bure2.incrementGrade(50);
-	} catch (std::exception & e)
-	{
+		bureaucrat.signForm(form);
+		bureaucrat.signForm(form2);
+	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
+	
+	std::cout << "\n===== PRINTING =====" << std::endl;
+	std::cout << bureaucrat << std::endl;
+	std::cout << form << std::endl;
+	std::cout << form2 << std::endl;
 
+	std::cout << "\n===== INCREMENT AND SIGNING =====" << std::endl;
 	try {
-		bure4.decrementGrade();
-	} catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	try {
-		Bureaucrat bure5("Arnold", -50);
-	} catch (std::exception & e)
-	{
+		bureaucrat.incrementGrade(110);
+		bureaucrat.signForm(form);
+		bureaucrat.signForm(form2);
+	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
 
 	std::cout << "\n===== PRINTING =====" << std::endl;
-	std::cout << bure1 << std::endl;
-	std::cout << bure2 << std::endl;
-	std::cout << bure3 << std::endl;
-	std::cout << bure4 << std::endl;
+	std::cout << bureaucrat << std::endl;
+	std::cout << form << std::endl;
+	std::cout << form2 << std::endl;
 
 	std::cout << "\n===== END =====" << std::endl;
 }
