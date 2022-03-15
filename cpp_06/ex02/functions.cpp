731cbee68cc6f59d19f7040b61c161f9aacb2283
aneuwald <aneuwald@gmail.com>
2022-03-15 01:28:12 +0000

    print on generate

--- cpp_06/ex02/functions.cpp
+++ cpp_06/ex02/functions.cpp
@@ -6,7 +6,7 @@
 /*   By: acanterg <acanterg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 12:40:43 by acanterg          #+#    #+#             */
-/*   Updated: 2022/03/15 01:24:27 by acanterg         ###   ########.fr       */
+/*   Updated: 2022/03/15 01:25:58 by acanterg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ Base * generate(void) {
 
 	switch (prob) {
 	case 0:
-		//std::cout << "A done" << std::endl;
+		std::cout << "A done" << std::endl;
 		return (new A());
 	case 1:
-		//std::cout << "B done" << std::endl;
+		std::cout << "B done" << std::endl;
 		return (new B());	
 	default:
-		//std::cout << "C done" << std::endl;
+		std::cout << "C done" << std::endl;
 		return (new C());
 	}
 	std::cout << "Nothing generated? WHAT?!?!!??" << std::endl;
@@ -54,17 +54,17 @@ void identify(Base* p) {
 
 void identify(Base& p) {
 	try {
-		dynamic_cast<A&>(p);
+		(void) dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
 		return;
 	}  catch (std::exception& e) {};
 	try {
-		dynamic_cast<B&>(p);
+		(void) dynamic_cast<B&>(p);
 		std::cout << "B" << std::endl;
 		return;
 	} catch (std::exception& e) {};
 	try {
-		dynamic_cast<C&>(p);
+		(void) dynamic_cast<C&>(p);
 		std::cout << "C" << std::endl;
 		return;
 	}  catch (std::exception& e) {};
